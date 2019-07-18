6ea26dbc595d1661f37daa80248a4dbcc1d3fdd7
m4riusz <sariuszm@gmail.com>
2019-07-18 20:16:58 +0200

    Initial Commit

---
+++ PuzzleAppRedux/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PuzzleAppRedux
+//
+//  Created by Mariusz Sut on 18/07/2019.
+//  Copyright © 2019 Mariusz Sut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
